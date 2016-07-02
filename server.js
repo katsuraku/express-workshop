7edd76fec6a7f7f5d4a952fa9c1a240d38333210
katsuraku <kirsten.jones@ft.com>
2016-07-02 14:02:05 +0100

    Text area for entering blog posts. Body-parser middleware making data in req.body available

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+// To access the req body when data has been POSTed to the server, need middleware.
+// body-parser parses data in the request and makes it available in req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
   console.log(req);
+});
+
+app.post("/create-post", function (req, res) {
+  res.redirect('/');
 });
 
 app.get("/:something", function (req, res) {
