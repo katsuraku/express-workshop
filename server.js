55d790e5792ae77e3b27c4f19047250df8ef6c02
katsuraku <kirsten.jones@ft.com>
2016-07-02 16:02:01 +0100

    Reads new blogposts and adds to a file of all of them

--- server.js
+++ server.js
@@ -4,28 +4,39 @@ var fs = require('fs');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// fs.readFile(__dirname + '/data/posts.json', function (error, file) {
-//  var parsedFile = JSON.parse(file);
-//  console.log(file.toString());
-//  });
-
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
-  console.log(req);
+  res.send('Hello!');  
+});
+
+app.get("/posts", function (req, res) {
+  res.sendFile(__dirname + '/data/posts.json');
 });
 
 app.post("/create-post", function (req, res) {
-  console.log(req.body);
-  var newPost = JSON.stringify(req.body);
-  fs.writeFile(__dirname + '/data/posts.json', newPost, function (error) {
-    if (error) {
-      console.log('There was this error', error);
-    }
+  fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+
+    var newPost = req.body.blogpost.trim();
+    console.log('newPost: ', newPost);
+
+    var currentPosts = JSON.parse(file);
+    console.log('currentPosts: ', currentPosts);
+    
+    var timeStamp = Date.now();
+    currentPosts[timeStamp] = newPost;
+
+    var stringifiedPosts = JSON.stringify(currentPosts);
+
+    fs.writeFile(__dirname + '/data/posts.json', stringifiedPosts, function (error) {
+      if (error) {
+        console.log('There was this error', error);
+      }
     console.log('I wrote the new blogpost into the file');
-  });
+ 
+    console.log('Updated posts list', stringifiedPosts);
+    });
+  });  
   res.redirect('/');
 });
 
